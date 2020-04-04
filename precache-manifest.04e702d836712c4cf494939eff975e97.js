e4efc87d6416b9f551c252ede351befba4897706
CrioDo <rahul@criodo.com>
2020-04-04 23:16:07 +0000

    Updates

---
+++ precache-manifest.04e702d836712c4cf494939eff975e97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55dd40aad2335633c2adbb928ad5b9b",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/index.html"
+  },
+  {
+    "revision": "30a85ab22f8358b9a917",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "1627f0542d52ca826c2a",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "30a85ab22f8358b9a917",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/js/2.c7d2dbc0.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/js/2.c7d2dbc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1627f0542d52ca826c2a",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/js/main.eac6329f.chunk.js"
+  },
+  {
+    "revision": "06dca30d5388a80f11ae",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/js/runtime-main.be68a659.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-tanvi-a17-iiits/static/media/crio-logo.63be71d0.svg"
+  }
+]);
